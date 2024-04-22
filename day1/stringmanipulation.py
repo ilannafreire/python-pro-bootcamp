b62b107bc65a5c5c857e65943bf2af4d85c0f804
Ilanna Freire <ilannafreiret@hotmail.com>
2024-04-22 08:15:10 -0300

    feat: day1 string manipulation

---
+++ day1/stringmanipulation.py
@@ -0,0 +1,9 @@
+print("Hello" + "" + "Ilanna")
+
+print("Hello, " +"Ilanna!")
+
+
+#'''String concatenation is simply taking those separate strings of characters and merging into one.'''
+#'''Its really important that you start of all your code at the beginning of line.'''
+ 
+ 
