35a76739cbfca6355efaf74df6ed275a1aa9d3b6
Ilanna Freire <ilannafreiret@hotmail.com>
2024-04-22 08:14:01 -0300

    feat: day1 printing

---
+++ day1/printmodifiers.py
@@ -0,0 +1,15 @@
+print('She said: "Hello" and then left ')
+
+#''' So how do you solve this? Well, a really simple way is to alternate the use of single and double quotes.'''
+
+print('She said: "Hello" and then left.')
+
+#'''Now, another alternative option'''
+
+print("She said: \"Hello\" and then left")
+
+
+
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
